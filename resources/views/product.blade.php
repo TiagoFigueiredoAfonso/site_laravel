0f2417f86bf6992d14ed8cfc40f31edfe6009a1b
Tiago <67653921+TiagoFigueiredoAfonso@users.noreply.github.com>
2022-06-28 11:38:38 -0400

    banco criado, dados adicionados, página welcome criada

--- resources/views/product.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.main')
-
-@section('title', 'product')
-
-@section('content')
-
-<p>exibindo o id {{ $id }}</p>
-
-@endsection
--- resources/views/products.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.main')
-
-@section('title', 'produtos')
-
-@section('content')
-
-<h1>produtos</h1>
-
-@endsection
