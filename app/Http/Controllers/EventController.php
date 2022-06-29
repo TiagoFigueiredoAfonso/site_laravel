f2aed7dec9a699b9e01cb58115da07d650f16bef
Tiago <67653921+TiagoFigueiredoAfonso@users.noreply.github.com>
2022-06-29 14:11:53 -0400

    aula 19, jason- alteração no model, adicionando column jason nas migrations, nullable()

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -25,6 +25,7 @@ class EventController extends Controller
         $event->city = $request->city;
         $event->private = $request->private;
         $event->description = $request->description;
+        $event->items = $request->items;//alteração no model, cast pra receber array
 
         // Image Upload
 
--- resources/views/events/show.blade.php
+++ resources/views/events/show.blade.php
@@ -15,7 +15,14 @@
                 <p class="events-participants"><ion-icon name="people-outline"></ion-icon>X Partipantes</p>
                 <p class="event-owner"><ion-icon name="star-outline"></ion-icon>Dono do Evento</p>
                 <a href="#" class="btn btn-primary" id="event-submit">Confirmar Presença</a>
+                <h3>O evento conta com:</h3>
+                <ul id="items-list">
+                    @foreach($event->items as $item)
+                        <li><ion-icon name="play-outline"></ion-icon><span>{{ $item }}</span></li>
+                    @endforeach
+                </ul>
             </div>
+
             <div class="col-md-12" id="description-container">
                 <p class="event-description">{{ $event->description }}</p>
             </div>
